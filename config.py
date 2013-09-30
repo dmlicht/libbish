3eb42b8324d5ec0fe624f2fddb8c6b74f0e4f221
David Lichtenberg <david.m.lichtenberg@gmail.com>
2013-09-30 14:28:22 -0400

    get database uri from env

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
-SQLITE_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'lib_working.db')
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', SQLITE_DATABASE_URI)
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', SQLALCHEMY_DATABASE_URI)
 DEBUG = True
