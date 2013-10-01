df7aac7d2e5f00bfcfef977282e1fe6839c19cc3
David Lichtenberg <david.m.lichtenberg@gmail.com>
2013-10-01 11:43:13 -0400

    heroku config

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
 if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
+    SQLITE_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', SQLALCHEMY_DATABASE_URI)
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', SQLITE_DATABASE_URI)
 DEBUG = True
